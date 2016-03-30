028557accfeecf3e8bbbdf9e4b6c5a48d6bb7c90
Anderson Ku <anderson.ku@amadeus.com>
2016-03-31 10:00:42 +1100

    Added my own bindings

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,4 +1,108 @@
+(defun xah-copy-line-or-region ()
+  "Copy current line, or text selection.
+When `universal-argument' is called first, copy whole buffer (but respect `narrow-to-region')."
+  (interactive)
+  (let (p1 p2)
+    (if (null current-prefix-arg)
+        (progn (if (use-region-p)
+                   (progn (setq p1 (region-beginning))
+                          (setq p2 (region-end)))
+                 (progn (setq p1 (line-beginning-position))
+                        (setq p2 (line-end-position)))))
+      (progn (setq p1 (point-min))
+             (setq p2 (point-max))))
+    (kill-ring-save p1 p2)))
+
+(defun xah-cut-line-or-region ()
+  "Cut current line, or text selection.
+When `universal-argument' is called first, cut whole buffer (but respect `narrow-to-region')."
+  (interactive)
+  (let (p1 p2)
+    (if (null current-prefix-arg)
+        (progn (if (use-region-p)
+                   (progn (setq p1 (region-beginning))
+                          (setq p2 (region-end)))
+                 (progn (setq p1 (line-beginning-position))
+                        (setq p2 (line-beginning-position 2)))))
+      (progn (setq p1 (point-min))
+             (setq p2 (point-max))))
+    (kill-region p1 p2)))
+
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "<f1>") 'help-command)
+(global-set-key (kbd "<f2>") 'xah-cut-line-or-region)  ; cut
+(global-set-key (kbd "<f3>") 'xah-copy-line-or-region) ; copy
+(global-set-key [C-f3] 'highlight-symbol-at-point)
+(global-set-key (kbd "<f4>") 'yank) ; paste
 (global-set-key [(f5)] 'recompile)
 (global-set-key [C-f5] 'compile)
+(global-set-key (kbd "<f8>") 'ggtags-find-tag-dwim)
+(global-set-key (kbd "<f9>") 'ff-get-other-file)
+(global-set-key [(f11)] 'kill-this-buffer)
+(global-set-key [(C-f11)] 'kill-this-buffer)
+(global-set-key [(f12)] 'delete-frame)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+
+(define-key global-map (kbd "C-z") 'undo-tree-undo)
+(define-key global-map (kbd "C-S-z") 'undo-tree-redo)
+
+(define-key global-map (kbd "M-RET") 'ace-jump-mode)
+(global-set-key [(control up)]  'move-line-up)
+(global-set-key [(control down)]  'move-line-down)
+(global-set-key [(control l)] 'kill-whole-line)
+(global-set-key [?\e deletechar] 'kill-word)
+
+(setq redisplay-dont-pause t)
+
+(remove-hook 'find-file-hooks 'vc-find-file-hook)
+(setq vc-handled-backends ())
+
+(require 'vlf-setup)
+
+(require 'goto-last-change)
+(global-set-key "\C-x\C-\\" 'goto-last-change)
+
+
+
+;; Save all tempfiles in $TMPDIR/emacs$UID/
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
+
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+(require 'ahg)
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
 
 (provide 'init-local)
